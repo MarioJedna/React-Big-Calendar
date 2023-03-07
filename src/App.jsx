f082a82100be259fe25cf27283764ddb8eefc390
MarioStation <lartu@seznam.cz>
2023-03-07 08:04:30 +0100

    calendar adding events working

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import enUS from "date-fns/locale/en-US";
+import "react-datepicker/dist/react-datepicker.css";
 
 const locales = {
   "en-US": enUS,
@@ -41,11 +42,41 @@ const events = [
 ];
 
 function App() {
+  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+  const [allEvents, setAllEvents] = useState(events);
+
+  function handleAddEvent() {
+    setAllEvents([...allEvents, newEvent]);
+  }
+
   return (
     <div className="App">
+      <h1>Calendar</h1>
+      <h2>Add New Event</h2>
+      <div>
+        <input
+          type="text"
+          placeholder="Add Title"
+          value={newEvent.title}
+          onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })}
+        />
+        <DatePicker
+          placeholderText="Start Date"
+          style={{ marginRight: "10px" }}
+          selected={newEvent.start}
+          onChange={(start) => setNewEvent({ ...newEvent, start })}
+        />
+        <DatePicker
+          placeholderText="End Date"
+          style={{ marginRight: "10px" }}
+          selected={newEvent.end}
+          onChange={(end) => setNewEvent({ ...newEvent, end })}
+        />
+        <button onClick={handleAddEvent}>Add Event</button>
+      </div>
       <Calendar
         localizer={localizer}
-        events={events}
+        events={allEvents}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 500 }}
